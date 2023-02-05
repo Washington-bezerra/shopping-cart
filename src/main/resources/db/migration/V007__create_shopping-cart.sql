d34483ffa718043e979bdbf543003738dd8bb3c5
Washington Bezerra <61431230+Washington-bezerra@users.noreply.github.com>
2023-02-04 21:40:12 -0300

    create shopping_cart table

---
+++ src/main/resources/db/migration/V007__create_shopping-cart.sql
@@ -0,0 +1,9 @@
+CREATE TABLE shopping_cart (
+    id INT PRIMARY KEY,
+    order_id uuid NOT NULL,
+    client_id INT REFERENCES client(id) NOT NULL,
+    item_id INT REFERENCES item(id) NOT NULL,
+    quantity_item INT NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL
+);
